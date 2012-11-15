c0cd51768a1a728b55c7d78fbba35c2aeda53d7c
Gabor Szabo <gabor@szabgab.com>
2012-11-15 09:51:26 +0200

    skeleton for test

---
+++ t/test_wc.py
@@ -0,0 +1,16 @@
+import unittest
+#import sys
+#sys.path.append('bin')
+#import wc
+
+class TestWC(unittest.TestCase):
+
+    def setUp(self):
+        pass
+        # create temporary file
+
+    def test_wc(self):
+        self.assertTrue(True)
+    
+if __name__ == '__main__':
+    unittest.main()
